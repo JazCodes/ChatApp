1b381b9a29bf449a4bcc7135687c7b9627be6cec
Jaskirat Billing <Billingjas22@gmail.com>
2019-02-19 12:59:10 -0800

    few changes

--- server.py
+++ server.py
@@ -1,16 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 from flask import request
 
+import sqlite3
+
 app = Flask(__name__)
+
+conn = sqlite3.connect('chat.db')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS messages(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	message TEXT)''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS user(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT)''')
+
+
+conn.commit()
+conn.close()
+
+
+
 
 @app.route('/')
 def home_page():
 	return render_template("index.html")
 
-@app.route('/string')
-def string():
 
-	return("dsad")
+@app.route('/chatapp')
+def fetch_all():
+	conn = sqlite3.connect('chat.db')
+	cur = conn.cursor()
+
+	cur.execute('''SELECT * FROM messages''')
+
+	rows = cur.fetchall()
+	return jsonify(rows)
+
 if __name__ == '__main__':
 	app.run()
--- static/chat.js
+++ static/chat.js
@@ -1,40 +1,22 @@
 $( document ).ready(function() {
-	console.log( "ready!" );
-	var randomStringData;
+    function fetch (){
 
-	$("#target").click(function() {
-		console.log("Handler for .click() called.");
-		var top10;
-		$.get( "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty", function( data ) {
-	  		top10 = data.slice(0,10);
-	  		console.log('Line 11', top10);
-	  		for(var i = 0; i < top10.length; i++) {
-	  			var currId = top10[i];
-	  			$.get(`https://hacker-news.firebaseio.com/v0/item/${currId}.json?print=pretty`, function ( data ) {
-	  				$(" #random-string" ).append( `<div> ${data['title']} </div>`);
+    	$.get("/chatapp", function (data){
+    		console.log(data);
 
-	  			});
+    		$(".messages").empty();
+    		data.forEach((x) => {
+    			$(".messages").append(`<div>${x}</div>`);
+    			
+    		});
+    	});
+    }
 
-	  		}
+    function init() {
+    	setInterval(function(){fetch(); }, 5000);
 
-		});
+    }
 
-
-	$("#second").click(function() {
-		console.log("Handler for .click() called.");
-		var top10;
-		$.get( "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty", function( data ) {
-	  		next10 = data.slice(11,20);
-	  		for(var i = 0; i < next10.length; i++) {
-	  			var currenId = next10[i];
-	  			$.get(`https://hacker-news.firebaseio.com/v0/item/${currenId}.json?print=pretty`, function ( data ) {
-	  				$(" #second-string" ).append( `<div> ${data['title']} </div>`);
-	  				console.log('line 35', next10);
-	  			});
-
-	  		}
-
-		});
-	});
+    init();
 });
-});
+
